f40edae26db556cc04d08a23b0fa1b7442efa553
Nazar Komar <nazarkomar13@gmail.com>
2021-02-13 17:00:22 +0200

    1.1

---
+++ App.js
@@ -0,0 +1,67 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+function General() {
+  return (
+    <View style={styles.center}>
+      {["Комар Назар", "Група ІО-81", "ЗК ІО-8115"].map(function(object) {
+        return <Text style={styles.center_text}>{object}</Text>;
+      })}
+    </View>
+  );
+}
+
+function  Item() {
+  return(<View></View>);
+}
+
+const Tab = createBottomTabNavigator();
+
+
+const App: () => React$Node = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        tabBarOptions={{
+          activeTintColor: "tomato",
+          inactiveTintColor: "gray",
+          labelStyle: {
+            fontSize: 20,
+          },
+
+        }}>
+        <Tab.Screen
+          name="General"
+          component={General}
+        />
+        <Tab.Screen
+          name="Item"
+          component={Item}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  center_text: {
+    textAlign: "center",
+  },
+  center: {
+    justifyContent: 'center',
+    alignItems: "center",
+    flex: 1,
+  },
+});
+
+export default App;
