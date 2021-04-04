0cd0d17e4a17194ee5c8435f08ef53f20588bd08
Nazar Komar <nazarkomar13@gmail.com>
2021-04-04 17:21:20 +0300

    lab4

--- App.js
+++ App.js
@@ -7,13 +7,18 @@
  */
 
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, TextInput, Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from '@react-navigation/stack';
 import Drawing from './src/Draw';
-import BooksList from './src/BooksList';
-
-function General() {
+import BookList from './src/BooksList';
+import DetailBook from "./src/DetailBook";
+import BookAdd from "./src/BookAdd";
+import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
+import { IconButton, Colors } from 'react-native-paper';
+function General(props) {
+  props.navigation.setOptions({})
   return (
     <View style={styles.center}>
       {["Комар Назар", "Група ІО-81", "ЗК ІО-8115"].map(function(object) {
@@ -24,27 +29,78 @@ function General() {
 }
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
+
+function getHeader(route) {
+  const routeName = getFocusedRouteNameFromRoute(route);
+
+  switch (routeName) {
+    case 'Drawing':
+      return false;
+    case 'General':
+      return false;
+    case 'BookList':
+      return true;
+  }
+}
+function getHeaderRight(route, navigation) {
+  const routeName = getFocusedRouteNameFromRoute(route);
+
+  switch (routeName) {
+    case 'Drawing':
+      return null;
+    case 'General':
+      return null;
+    case 'BookList':
+      return <IconButton
+        icon="plus"
+        color={Colors.lightBlueA700}
+        size={35}
+        onPress={() => navigation.navigate('BookAdd')}
+      />;
+  }
+}
+
+function TabGeneral({ navigation, route }) {
+  React.useLayoutEffect(() => {
+    navigation.setOptions({ headerShown: getHeader(route),
+      headerTitle: "",
+      headerRight: () => (
+       getHeaderRight(route, navigation)
+      ),
+    });
+
+  }, [navigation, route]);
+
+  return (
+    <Tab.Navigator
+      tabBarOptions={{
+        activeTintColor: "tomato",
+        inactiveTintColor: "gray",
+        labelStyle: {
+          fontSize: 20,
+        },
+      }}>
+      <Tab.Screen name="General" component={General} />
+      <Tab.Screen name="Drawing" component={Drawing} />
+      <Tab.Screen name="BookList" component={BookList}/>
+    </Tab.Navigator>
+  );
+}
 
 const App: () => React$Node = () => {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        tabBarOptions={{
-          activeTintColor: "tomato",
-          inactiveTintColor: "gray",
-          labelStyle: {
-            fontSize: 20,
-          },
-
-        }}>
-        <Tab.Screen
-          name="General"
-          component={General}
-        />
-        <Tab.Screen name="Drawing" component={Drawing} />
-        <Tab.Screen name="BooksList" component={BooksList} />
-      </Tab.Navigator>
+    <NavigationContainer >
+      <Stack.Navigator
+        screenOptions={{
+          headerTitle: ""
+        }}
+      >
+        <Stack.Screen  name="TabGeneral" component={TabGeneral} />
+        <Stack.Screen name="DetailBook" component={DetailBook} />
+        <Stack.Screen name="BookAdd" component={BookAdd} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
@@ -58,6 +114,11 @@ const styles = StyleSheet.create({
     alignItems: "center",
     flex: 1,
   },
+  iconContainer: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    width: 120
+  }
 });
 
 export default App;
--- src/BooksList.js
+++ src/BooksList.js
@@ -1,154 +1,202 @@
-import React from 'react';
-import {
-  StyleSheet,
-  View,
-  Text,
-  FlatList,
-  Image,
-  SafeAreaView,
-} from 'react-native';
+import React  from "react";
+import { Image, StyleSheet, Text, TouchableHighlight, View, Button, Animated, TouchableOpacity } from "react-native";
+import { ListView } from 'realm/react-native';
 
-const booksObj = require('./TextFiles/BooksList.json');
+import newBooks from './BooksParse';
+import { SearchBar } from "react-native-elements";
+import Swipeable from 'react-native-gesture-handler/Swipeable'
 
-const books = booksObj['books'];
-const newBooks = [];
-
-class Book {
-  constructor(title, subtitle, isbn13, price, image) {
-    if (title.length > 50) {
-      this.title = title.slice(0, 55) + '...';
-    } else {
-      this.title = title;
-    }
-    this.subtitle = subtitle;
-    this.isbn13 = isbn13;
-    this.price = price;
-    this.image = image;
-  }
-}
-
-for (let bookObj of books) {
-  const newBook = new Book(
-    bookObj.title,
-    bookObj.subtitle,
-    bookObj.isbn13,
-    bookObj.price,
-    bookObj.image,
-  );
-  newBooks.push(newBook);
-}
-
-const BookImage = ({book}) => {
-  switch (book.title) {
-    case 'iOS Components and Frameworks':
-      return (
-        <Image
-          source={require('../img/Image_01.png')}
-          style={styles.bookImage}
-        />
-      );
-    case 'Learning iOS Development':
-      return (
-        <Image
-          source={require('../img/Image_02.png')}
-          style={styles.bookImage}
-        />
-      );
-    case 'Beginning iOS Programming':
-      return (
-        <Image
-          source={require('../img/Image_03.png')}
-          style={styles.bookImage}
-        />
-      );
-    case 'Beginning iOS 5 Games Development':
-      return (
-        <Image
-          source={require('../img/Image_05.png')}
-          style={styles.bookImage}
-        />
-      );
-    case 'More iOS 6 Development':
-      return (
-        <Image
-          source={require('../img/Image_06.png')}
-          style={styles.bookImage}
-        />
-      );
-    case 'Beginning iOS 6 Development':
-      return (
-        <Image
-          source={require('../img/Image_07.png')}
-          style={styles.bookImage}
-        />
-      );
-    case 'Beginning iOS 7 Development':
-      return (
-        <Image
-          source={require('../img/Image_08.png')}
-          style={styles.bookImage}
-        />
-      );
-    case 'iOS 6 Programming Cookbook':
-      return (
-        <Image
-          source={require('../img/Image_10.png')}
-          style={styles.bookImage}
-        />
-      );
-    default:
-      return (
-        <Image
-          source={require('../img/Image_Default.png')}
-          style={styles.bookImage}
-        />
-      );
-  }
+const data = [];
+// у нас немає можливості передавати зміну в require
+const img_path_section = {
+  "Image_01.png": require("../img/Image_01.png"),
+  "Image_02.png": require("../img/Image_02.png"),
+  "Image_03.png": require("../img/Image_03.png"),
+  "Image_05.png": require("../img/Image_05.png"),
+  "Image_06.png": require("../img/Image_06.png"),
+  "Image_07.png": require("../img/Image_07.png"),
+  "Image_08.png": require("../img/Image_08.png"),
+  "Image_10.png": require("../img/Image_10.png"),
 };
 
-const BookItem = ({item}) => {
-  return (
-    <View style={styles.item}>
-      <BookImage book={item} style={styles.bookImageContainer} />
-      <View style={styles.textContainer}>
-        <Text style={styles.title}>{item.title}</Text>
-        <Text style={styles.subtitle}>{item.subtitle}</Text>
-        <Text style={styles.price}>{item.price}</Text>
-      </View>
-    </View>
-  );
-};
 
-const BooksList = () => {
-  const renderItem = ({item}) => {
-    return <BookItem item={item} />;
+
+
+
+class BookList extends React.Component {
+
+
+  constructor(props) {
+    super(props);
+    let ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => (r1 !== r2) });
+    this.state = { dataSource: ds.cloneWithRows(newBooks) };
+  }
+
+  row: Array<any> = [];
+
+  componentWillMount = () => {
+    this.setState({ dataSource: this.state.dataSource.cloneWithRows(newBooks) });
   };
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={newBooks}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.isbn13}
-      />
-    </SafeAreaView>
-  );
-};
+  deleteBook = (title, index) => {
+    newBooks.forEach(function(item, index) {
+      if(item.title === title){
+        delete newBooks[index]}
+    });
+    this.row[index].close();
+    this.componentWillMount();
+  }
+
+  RightActions = (progress, dragX, height, rowData, index) => {
+    const scale = dragX.interpolate({
+      inputRange: [0, 0],
+      outputRange: [1.2, 0]
+    })
+    return (
+      <>
+        <TouchableOpacity onPress={() => this.deleteBook(rowData.title, index)}>
+          <View style={swipeableStyle(height)}>
+            <Animated.Text
+              style={{
+
+                color: 'white',
+                paddingHorizontal: 10,
+                fontWeight: '600',
+                transform: [{ scale }]
+              }}>
+              Delete
+            </Animated.Text>
+          </View>
+        </TouchableOpacity>
+      </>
+    )
+  }
+
+
+  _renderRow(rowData, index) {
+    let img_path;
+    try {
+      img_path = img_path_section[rowData.image];
+    } catch (e) {
+      img_path = require("../img/Image_Default.png");
+    }
+    let height
+    height = (rowData.title.length < 50) ? 160 : 160 + ((Math.floor(rowData.title.length)/25)-4)*25
+    return (
+      <Swipeable ref={ref => this.row[index] = ref} friction={3} overshootRight={false} overshootFriction={1} renderRightActions={(progress, dragX) => this.RightActions(progress, dragX, height, rowData, index)}>
+      <TouchableHighlight onPress={this.OpenSecondActivity.bind(this, rowData)} underlayColor="#fff7"
+                                 style={containerStyle(height)} >
+        <View style={itemStyle(height)}>
+          <Image style={styles.bookImage} source={img_path} />
+          <View style={styles.textContainer}>
+            <Text style={styles.title}>{rowData.title}</Text>
+            <Text style={styles.subtitle}>{rowData.subtitle}</Text>
+            <Text style={styles.price}>{rowData.price}</Text>
+          </View>
+        </View>
+
+      </TouchableHighlight>
+    </Swipeable>);
+  }
+
+  OpenSecondActivity(rowData) {
+    this.props.navigation.navigate("DetailBook", { ListViewClickItemHolder: rowData });
+  }
+
+  updateSearch = (search) => {
+    try {
+      search= search.replace(/\s/g, '');
+      if(search !== ''){
+        const newData = newBooks.filter(function (item) {
+          const itemData = item.title
+            ? item.title.toUpperCase()
+            : ''.toUpperCase();
+          const textData = search.toUpperCase();
+          return itemData.indexOf(textData) > -1;
+        });
+        this.setState({ dataSource: this.state.dataSource.cloneWithRows(newData) });
+      }
+      else{
+        this.componentWillMount();
+      }
+    }catch (e) {
+      this.componentWillMount();
+    }
+  };
+
+  componentDidMount() {
+    const { navigation } = this.props;
+
+    this.focusListener = navigation.addListener('focus', () => {
+      this.setState({ dataSource: this.state.dataSource.cloneWithRows(newBooks) });
+    });
+  }
+
+  componentWillUnmount() {
+    if (this.focusListener != null && this.focusListener.remove) {
+      this.focusListener.remove();
+    }
+  }
+
+  render() {
+    const { search } = this.state;
+    return (
+
+      <>
+        <SearchBar
+          searchIcon={{ size: 24 }}
+          platform="android"
+          lightTheme = {true}
+          round = {true}
+          containerStyle={{height: 50 }}
+          onChangeText={this.updateSearch}
+          onClear={this.updateSearch}
+          value={search}
+        />
+        <ListView removeClippedSubviews={false} dataSource={this.state.dataSource} renderRow={(rowData,sectionID, rowID, higlightRow) => this._renderRow(rowData, rowID)} />
+        </>
+    );
+  }
+}
+
+
+const containerStyle = function(height) {
+  return {
+
+    height: height,
+    borderColor: "#ddd",
+    flexDirection: "row",
+  }
+}
+
+const itemStyle = function(height) {
+  return {
+    padding: 1,
+    marginVertical: 1,
+    marginHorizontal: 1,
+    height: height,
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "white",
+
+  }
+}
+
+const swipeableStyle = function(height) {
+  return {
+    padding: 1,
+    marginVertical: 1,
+    marginHorizontal: 1,
+    backgroundColor: 'red',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: height,
+    width: 90,
+  }
+}
+
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  item: {
-    padding: 10,
-    marginVertical: 8,
-    marginHorizontal: 8,
-    height: 160,
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: 'white',
-    position: 'relative',
-  },
   textContainer: {
     flex: 1,
   },
@@ -161,19 +209,14 @@ const styles = StyleSheet.create({
   },
   price: {
     fontSize: 18,
-    fontWeight: '700',
+    fontWeight: "700",
     flex: 1,
-    alignSelf: 'flex-end',
-    position: 'absolute',
-    bottom: '10%',
+    position: "absolute",
+    bottom: "10%",
   },
   bookImage: {
     height: 150,
     width: 120,
-  },
-  bookImageContainer: {
-    flex: 1,
-  },
+  }
 });
-
-export default BooksList;
+export default BookList;
